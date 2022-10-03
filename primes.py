b88e5fe4d4615c22ce3124c4d0afd89a397e5137
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-10-03 21:27:58 +0100

    first try

--- primes.py
+++ primes.py
@@ -3,4 +3,27 @@
 
 def primes(number_of_primes):
     list = []
+    list.append(2)
+    check = True
+    counter = list[0]+1
+    if number_of_primes <= 0:
+        raise ValueError()
+    elif number_of_primes == 1:
+        return list
+
+    else:
+        while len(list) < number_of_primes:
+            for x in list:
+                if counter % x:
+                    check = True
+                    continue
+                else:
+                    check = False
+                    break
+            if check:
+                list.append(counter)
+            counter += 2
+    print(list)
     return list
+
+primes(10)
